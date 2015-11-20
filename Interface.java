7c97224f61196a6693963ac0829c60ae2e2df7f1
Namohysip <higa.noah@gmail.com>
2015-11-20 00:51:49 -1000

    Update Interface.java
    Made circles transparent, added titles to each circle (Let's just keep the circle feeds static) and also made it appear to the left-middle of the screen.

--- Interface.java
+++ Interface.java
@@ -21,9 +21,9 @@ public class Interface {
 		 * The height and width of the window, currently set at 300, 300. May be
 		 * resized in the future.
 		 */
-		private static final int WIDTH = 300;
-		private static final int HEIGHT = 300;
-		private int radius = 0;
+		private static final int WIDTH = 150;
+		private static final int HEIGHT = 150;
+		private int radius = 10;
 
 		public int getWindowWidth() {
 			return WIDTH;
@@ -42,10 +42,10 @@ public class Interface {
 		 */
 		public void addCircles() {
 
-			circles.add(new MyCircle(150, 150, radius, Color.GREEN));
-			circles.add(new MyCircle(30, 30, radius, Color.BLUE));
-			circles.add(new MyCircle(150, 30, radius, Color.MAGENTA));
-			circles.add(new MyCircle(30, 150, radius, Color.RED));
+			circles.add(new MyCircle(100, 100, radius, new Color(0,1,0,0.1f),0));
+			circles.add(new MyCircle(15, 15, radius, new Color(0,0,1,0.1f),1));
+			circles.add(new MyCircle(100, 15, radius, new Color(1,1,0,0.1f),2));
+			circles.add(new MyCircle(15, 100, radius, new Color(1,0,0,0.1f),3));
 			// circles.add(new MyCircle(90, 90, radius, Color.ORANGE));
 		}
 
@@ -73,10 +73,15 @@ public class Interface {
 		 */
 		protected void paintComponent(Graphics g) {
 			// super.paintComponent(g);
-			circles.get(0).updateCircles();
+			//circles.get(0).updateCircles();
 			for (MyCircle circle : circles) {
+				circle.updateCircles();
 				circle.drawCircle(g);
 			}
+		}
+		
+		public void setRadius(int circle, int r){
+			circles.get(circle).setRadius(r);
 		}
 	}
 
@@ -88,26 +93,33 @@ public class Interface {
 		private int y;
 		private Color backgroundColour;
 		private int radius;
+		private int feed;
+		private String[] titles = {"HNN","NPR","BBC","CNN"};
 
 		public void updateCircles() {
-			radius = Interpreter.HowManyEntries(0);
+			radius = Interpreter.HowManyEntries(feed);
 			x = trueX - radius / 4;
 			y = trueY - radius / 4;
+			if(radius < 1){
+				radius = 1;
+			}
 		}
 
 		public void setRadius(int i) {
 			radius = i;
-
+			x = trueX - radius / 2;
+			y = trueY - radius / 2;
 		}
 
-		public MyCircle(int x, int y, int radius, Color backgroundColour) {
+		public MyCircle(int x, int y, int radius, Color backgroundColour, int feed) {
 			this.trueX = x;
 			this.trueY = y;
 			this.radius = radius;
 
-			this.x = trueX - radius / 4;
-			this.y = trueY - radius / 4;
+			this.x = trueX - radius / 2;
+			this.y = trueY - radius / 2;
 			this.backgroundColour = backgroundColour;
+			this.feed = feed;
 		}
 
 		/**
@@ -118,6 +130,8 @@ public class Interface {
 		public void drawCircle(Graphics g) {
 			g.setColor(backgroundColour);
 			g.fillOval(x, y, radius, radius);
+			g.setColor(new Color(0,0,0,0.1f));
+			g.drawString(titles[feed], x+radius/4, (int) (y+radius/1.5));
 		}
 
 	}
@@ -142,20 +156,32 @@ public class Interface {
 		frame.setContentPane(drawingBoard);
 		frame.pack();
 		frame.addMouseListener(new MouseManager());
+		frame.setAlwaysOnTop(true);
+		frame.setTitle("Multi-News-Network");
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(0, dim.height/2-frame.getSize().height/2);
 		frame.setVisible(true);
 	}
+
 
 	public static void main(String[] args) {
 		ArrayList<URL> list = new ArrayList<URL>();
 		try {
 			list.add(new URL(
 					"http://www.hawaiinewsnow.com/Global/category.asp?C=3497&clienttype=rss"));
+
+			Interpreter.newFeed();
+			list.add(new URL( "http://www.npr.org/rss/rss.php?id=1001"));
+			Interpreter.newFeed();
+			list.add(new URL( "http://feeds.bbci.co.uk/news/rss.xml"));
+			Interpreter.newFeed();
+			list.add(new URL( "http://rss.cnn.com/rss/cnn_topstories.rss"));
+			Interpreter.newFeed();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Gatherer.setURLs(list);
-		Interpreter.newFeed();
 		refresher(0);
 		Runnable runnable = new Runnable() {
 			@Override
@@ -166,7 +192,7 @@ public class Interface {
 		EventQueue.invokeLater(runnable);
 		while (true) {
 			refresher(1 * 1000);
-			drawingBoard.repaint();
+			//drawingBoard.repaint();
 			drawingBoard.setVisible(false);
 			drawingBoard.setVisible(true);
 		}
@@ -179,7 +205,6 @@ public class Interface {
 
 		}
 		Interpreter.setInfo();
-		//Interpreter.setDate(new Date(), 0);
 	}
 
 }
