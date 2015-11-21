9c6de7f20c6b9f776ab6a191bf522f1cbf2157c3
Namohysip <higa.noah@gmail.com>
2015-11-20 14:56:46 -1000

    Update MouseManager.java
    Clicking on the circle now creates a new window that displays the stories. Working on new window functionality.

--- MouseManager.java
+++ MouseManager.java
@@ -3,9 +3,16 @@
  */
 package main;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * @author Noah Higa
@@ -13,7 +20,10 @@ import java.awt.event.MouseMotionListener;
  */
 public class MouseManager implements MouseListener, MouseMotionListener {
 
-	/* (non-Javadoc)
+	private static boolean[] open = {false,false,false,false};
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
 	 */
 	@Override
@@ -21,31 +31,56 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 		int height = Interface.getDrawingBoard().getHeight();
 		int width = Interface.getDrawingBoard().getWidth();
 		System.out.println("Mouse clicked!");
-		if(e.getX() < width / 2){
-			if ( e.getY() < height / 2){
-				//do stuff with the blue circle
-				System.out.println("Clicked on blue!");
-			}
-			else {
+		if (e.getX() < width / 2) {
+			if (e.getY() < height / 2) {
+				// do stuff with the blue circle
+				System.out.println("Clicked on NPR!");
+				Interpreter.setDate(new Date(), 1);
 
-				System.out.println("Clicked on red!");
-			}
-		}
-		else {
-			if (e.getY() < height / 2){
-				//do stuff with the magenta circle
+				displayList(1);
+			} else {
 
-				System.out.println("Clicked on magenta!");
-			}
-			else {
-				//do stuff with the green circle
+				System.out.println("Clicked on CNN!");
+				Interpreter.setDate(new Date(), 3);
 
-				System.out.println("Clicked on green!");
+				displayList(3);
+			}
+		} else {
+			if (e.getY() < height / 2) {
+				// do stuff with the magenta circle
+
+				System.out.println("Clicked on BBC!");
+				Interpreter.setDate(new Date(), 2);
+
+				displayList(2);
+			} else {
+				// do stuff with the green circle
+				Interpreter.setDate(new Date(), 0);
+				System.out.println("Clicked on HNN!");
+				displayList(0);
+				
 			}
 		}
 	}
 
-	/* (non-Javadoc)
+	private static void displayList(int feed){
+		JFrame frame = new JFrame("AmbientRSS - Korall");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		HeadlineList list = new HeadlineList(feed);
+		JPanel contentPane = new JPanel();
+		contentPane.add(list);
+		frame.setContentPane(list);
+		frame.pack();
+		frame.addMouseListener(new MouseManager());
+		frame.setAlwaysOnTop(true);
+		frame.setTitle("New Stories");
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(0, dim.height/2-frame.getSize().height/2);
+		frame.setVisible(true);
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
 	 */
 	@Override
@@ -54,8 +89,11 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
 	 */
 	@Override
 	public void mouseReleased(MouseEvent e) {
@@ -63,7 +101,9 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
 	 */
 	@Override
@@ -72,7 +112,9 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
 	 */
 	@Override
@@ -84,12 +126,12 @@ public class MouseManager implements MouseListener, MouseMotionListener {
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		
+
 	}
 
 }
