0dc38871d65edb23f6603d4cc1185c54423b67f0
Namohysip <higa.noah@gmail.com>
2015-11-21 00:13:56 -1000

    Create LinkClicker.java
    Mouse Listener that launches links based on where you click on the frame

---
+++ LinkClicker.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.awt.Desktop;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class LinkClicker implements MouseListener {
+
+	HeadlineList list = null;
+	public LinkClicker(HeadlineList list){
+		super();
+		this.list = list;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(e.getY() < ((list.getLinks().size() + 1) * 20) + 40 && e.getY() > 60){
+			launchLink(((e.getY() - 40) / 20) - 1);
+		}
+
+	}
+
+	private void launchLink(int i){
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop()
+				: null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(list.getLinks().get(i).toURI());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
