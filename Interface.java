037f30411a13a49e876ce3bc71dba109cab810e4
Namohysip <higa.noah@gmail.com>
2015-12-01 14:26:07 -1000

    Update Interface.java
    Made the refresher cycles more efficient using Thread.sleep

--- Interface.java
+++ Interface.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -12,9 +13,6 @@ import java.util.List;
 import javax.swing.*;
 
 public class Interface {
-
-	
-	
 
 	private static final int GAP = 1;
 	private static Interface i = new Interface();
@@ -39,16 +37,15 @@ public class Interface {
 		frame.setAlwaysOnTop(true);
 		frame.setTitle("Multi-News-Network");
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		frame.setLocation(0, dim.height/2-frame.getSize().height/2);
+		frame.setLocation(0, dim.height / 2 - frame.getSize().height / 2);
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
-			public void windowClosing(WindowEvent e){
+			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
 		frame.setVisible(true);
 	}
-
 
 	public static void main(String[] args) {
 		ArrayList<URL> list = new ArrayList<URL>();
@@ -57,18 +54,24 @@ public class Interface {
 					"http://www.hawaiinewsnow.com/Global/category.asp?C=3497&clienttype=rss"));
 
 			Interpreter.newFeed();
-			list.add(new URL( "http://www.npr.org/rss/rss.php?id=1001"));
+			list.add(new URL("http://www.npr.org/rss/rss.php?id=1001"));
 			Interpreter.newFeed();
-			list.add(new URL( "http://feeds.bbci.co.uk/news/rss.xml"));
+			list.add(new URL("http://feeds.bbci.co.uk/news/rss.xml"));
 			Interpreter.newFeed();
-			list.add(new URL( "http://rss.cnn.com/rss/cnn_topstories.rss"));
+			list.add(new URL("http://rss.cnn.com/rss/cnn_topstories.rss"));
 			Interpreter.newFeed();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Gatherer.setURLs(list);
-		refresher(0);
+		try {refresher(0);
+		
+		}
+		catch (Exception e){
+			System.out.println("Error: Cannot connect to Internet");
+			System.exit(1);
+		}
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
@@ -77,23 +80,27 @@ public class Interface {
 		};
 		EventQueue.invokeLater(runnable);
 		while (true) {
-			try { refresher(600 * 1000);
-			//drawingBoard.repaint();
-			drawingBoard.setVisible(false);
-			drawingBoard.setVisible(true);
-			}
-			catch(Exception e){
-				
+			try {
+				refresher(600 * 1000);
+				// drawingBoard.repaint();
+				drawingBoard.setVisible(false);
+				drawingBoard.setVisible(true);
+			} catch (Exception e) {
+				System.out.println("Connection error");
 			}
 		}
 
 	}
 
-	public static void refresher(long cycle) {
-		long lastCheck = System.currentTimeMillis();
-		while (lastCheck + cycle > System.currentTimeMillis()) {
-
+	public static void refresher(long cycle) throws ConnectException{
+		try {
+			Thread.sleep(cycle);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		
 		Interpreter.setInfo();
 	}
 
