e665cb52d7e3cfb0dbc9e0a4a085ab5036203765
Namohysip <higa.noah@gmail.com>
2015-12-01 14:28:04 -1000

    Update HeadlineList.java
    Added gray boxes to improve readability

--- HeadlineList.java
+++ HeadlineList.java
@@ -2,11 +2,14 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.net.URL;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class HeadlineList extends JPanel {
 
@@ -14,18 +17,20 @@ public class HeadlineList extends JPanel {
 	private ArrayList<URL> links = null;
 	private int feed = -1;
 
-	public ArrayList<String> getEntries(){
+	public ArrayList<String> getEntries() {
 		return entries;
 	}
-	
-	public ArrayList<URL> getLinks(){
+
+	public ArrayList<URL> getLinks() {
 		return links;
 	}
+
 	public HeadlineList(int feed) {
 		super();
 		entries = Interpreter.getEntries(feed);
 		links = Interpreter.getLinks(feed);
 		this.feed = feed;
+		this.setFont(new Font("Times New Roman", 1, 18));
 	}
 
 	/**
@@ -33,17 +38,24 @@ public class HeadlineList extends JPanel {
 	 */
 	protected void paintComponent(Graphics g) {
 		g.setColor(Color.BLACK);
-		for(int i = 0; i < entries.size(); i++){
-			String entry = entries.get(i);
-			if (entry.length() > 70){
+		g.drawString("New stories: " + Integer.toString(entries.size()), 250, 20);
+		for (int i = 0; i < entries.size(); i++) {
+			String entry = Integer.toString(i+1) + ".   " + entries.get(i);
+			if (entry.length() > 70) {
 				entry = entry.substring(0, 67) + "...";
 			}
-			g.drawString(entry, 10, 20*i + 40);
+		    if(i % 2 == 1){
+		    	g.setColor(new Color(200,200,200));
+		    	g.fillRect(0, 25 * i + 25, 1000, 25);
+		    	g.setColor(Color.BLACK);
+		    }
+		    
+			g.drawString(entry, 10, 25 * i + 40);
 		}
 	}
 
 	public Dimension getPreferredSize() {
-		return new Dimension(400, 400);
+		return new Dimension(600, entries.size() * 25 + 50);
 	}
 
 	/**
